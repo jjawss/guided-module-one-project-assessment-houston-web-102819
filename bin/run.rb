2be3a4aff425bbd67ef357e74072d7177df1b2b4
David Jaworski <jaws360@gmail.com>
2019-11-13 14:13:00 -0600

    wrapped menus into methods

--- bin/run.rb
+++ bin/run.rb
@@ -3,25 +3,13 @@ require_relative '../config/environment'
 
 class MovieApp
 
-  attr_accessor :user
+  attr_accessor :prompt, :user, :users_movie, :users_option
+  
+  @prompt = TTY::Prompt.new
 
-  prompt = TTY::Prompt.new
-
-  # def check_or_create(name)
-  #   user = User.find_by({ name: name })
-  #   if user == nil
-  #     return User.create(name: "#{name}")
-  #   else
-  #     return puts "Hello #{name}, what would you like to do?"
-  #   end
-
-    # User.all.each do |user_name|
-    #     if user_name.name == name
-    #     return puts "Hello #{name}, what would you like to do?"
-    #     end
-    #   end
-    #   return User.create(name: "#{name}")
-    # end
+########################################################################################
+#Methods
+########################################################################################
 
   def self.check_or_create(name)
     prompt = TTY::Prompt.new
@@ -34,44 +22,59 @@ class MovieApp
     end
   end
 
-  #-------------------------------------------------------
+  def self.welcome_frame
+    puts "Welcome to Movie App!"
 
-  #Frame 1
-  puts "Welcome to Movie App!"
+    puts "What is your name?"
+    current_user = gets.chomp
 
-  puts "What is your name?"
-  current_user = gets.chomp
-
-  #Frame 2
-  user_choice = check_or_create(current_user)
-
-
-  movie_titles = Movie.all.map do |movie|
-    movie.title
+    @user_choice = check_or_create(current_user)
   end
 
-#THE WAY IT WAS ORIGINALLY, ONE TREE OF OPTIONS
-  # if user_choice == "Select a movie"
-  #   users_movie = prompt.select("What movie would you like to see?", movie_titles)
-  # else 
-  #   prompt.select("User Options", ["Update name", "Delete user"])
-  # end
-
-  if user_choice == "Select a movie"
-    users_movie = prompt.select("What movie would you like to see?", movie_titles)
-  else 
-    users_option = prompt.select("User Options", ["Update name", "Delete user"])
+  def self.movie_titles
+    Movie.all.map do |movie|
+      movie.title
+    end
   end
 
-  if users_movie != nil  #SELECT A TICKET PATH
-    location_names = Location.all.map do |location|
+  def self.movie_menu
+    prompt = TTY::Prompt.new
+    @users_movie = prompt.select("What movie would you like to see?", movie_titles)
+  end
+  
+  def self.options_menu
+    prompt = TTY::Prompt.new
+    @users_option = prompt.select("User Options", ["Update name", "Delete user"])
+  end
+
+  def self.location_names
+    #prompt = TTY::Prompt.new
+    @prompt
+    Location.all.map do |location|
       location.name
     end
+  end
+########################################################################################
+#USER APP
+########################################################################################
+  welcome_frame
 
-    users_location = prompt.select("Select a location", location_names)
+  if @user_choice == "Select a movie"
+    movie_menu
+  else 
+    options_menu
+  end
+
+  #################
+  #MOVIE MENU PATH#
+  #################
+  if @users_movie != nil  #SELECT A TICKET PATH
+    location_names
+
+    users_location = @prompt.select("Select a location", location_names)
 
 
-    movie_object = Movie.where(title: users_movie)
+    movie_object = Movie.where(title: @users_movie)
     location_object = Location.where(name: users_location)
     tickets_for_movie_location_object = Ticket.where(movie: movie_object, location: location_object)
 
@@ -81,41 +84,29 @@ class MovieApp
     end
 
 
-    users_ticket = prompt.select("Select a time", ticket_labels)
+    users_ticket = @prompt.select("Select a time", ticket_labels)
 
     puts "You're going to #{users_ticket.location.name} to see #{users_ticket.movie.title} at #{users_ticket.time}."
 
     users_ticket.user = @user
     users_ticket.save
 
-  elsif users_option == "Update name"
+
+##############
+#Profile Menu#
+##############
+  elsif @users_option == "Update name"
     puts "What would you like your name to be?"
     updated_name = gets.chomp
     @user.update(name: updated_name)
   
-  elsif users_option == "Delete user" 
-    delete_user_choice = prompt.select("Are you sure you want to Delete your user name?", ["yes", "no"])
+  elsif @users_option == "Delete user" 
+    delete_user_choice = @prompt.select("Are you sure you want to Delete your user name?", ["yes", "no"])
     if delete_user_choice == "yes"
       @user.destroy
     else delete_user_choice == "no"
       #return to the beginning of the app
+      welcome_frame
     end
   end
 end
-
-
-
-
-
-
-
-
-# current_user_object = User.where(name: current_user)
-
-# users_ticket.user = current_user_object
-
-
-
-
-
-
